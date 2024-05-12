c22234f084290bf222e5b225e61133ed07d09e1f
Alex-Trauthman <alextrauthman.full@gmail.com>
2024-05-12 17:48:16 -0300

    dto

--- src/main/java/com/alex/springmongodb/resources/UserResource.java
+++ src/main/java/com/alex/springmongodb/resources/UserResource.java
@@ -3,6 +3,7 @@ package com.alex.springmongodb.resources;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -11,6 +12,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.alex.springmongodb.domain.User;
+import com.alex.springmongodb.dto.UserDTO;
 import com.alex.springmongodb.services.UserService;
 
 @RestController
@@ -19,8 +21,9 @@ public class UserResource {
 	@Autowired
 	private UserService service;
 	@GetMapping
-	public ResponseEntity<List<User>> findAll(){
-		List<User> users = service.findAll();
+	public ResponseEntity<List<UserDTO>> findAll(){
+		List<User> list = service.findAll();
+		List<UserDTO> users = list.stream().map(x -> new UserDTO(x)).collect(Collectors.toList());
 		return ResponseEntity.ok().body(users);
 	}
 }
