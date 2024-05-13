7e237e84f3f3ac119e8cba52245de3518a4f994a
Alex-Trauthman <alextrauthman.full@gmail.com>
2024-05-12 21:35:23 -0300

    added posts

--- src/main/java/com/alex/springmongodb/config/Instantiation.java
+++ src/main/java/com/alex/springmongodb/config/Instantiation.java
@@ -1,26 +1,39 @@
 package com.alex.springmongodb.config;
 
+import java.text.SimpleDateFormat;
 import java.util.Arrays;
+import java.util.TimeZone;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
 
+import com.alex.springmongodb.domain.Post;
 import com.alex.springmongodb.domain.User;
+import com.alex.springmongodb.repositories.PostRepository;
 import com.alex.springmongodb.repositories.UserRepository;
 
 @Configuration
 public class Instantiation implements CommandLineRunner {
 	@Autowired
 	private UserRepository userRepository;
+	
+	@Autowired
+	private PostRepository postRepository;
 	@Override
 	public void run(String... args) throws Exception {
+		postRepository.deleteAll();
 		userRepository.deleteAll();
 		
+		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+		sdf.setTimeZone(TimeZone.getTimeZone("GMT"));
 		User maria = new User(null, "Maria Brown", "maria@gmail.com");
 		User alex = new User(null, "Alex Green", "alex@gmail.com");
 		User bob = new User(null, "Bob Grey", "bob@gmail.com");
 		
+		Post pos1 = new Post(null, sdf.parse("21/03/2018"), "Partiu sp", "vo pra sp", maria);
+		Post pos2 = new Post(null, sdf.parse("11/05/2024"), "Enchentes no rs", "Doem por favor!", alex);
+		postRepository.saveAll(Arrays.asList(pos1,pos2));
 		userRepository.saveAll(Arrays.asList(maria,alex,bob));
 	}
 
