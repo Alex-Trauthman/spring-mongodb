93fee8155d8d013269df3c3d77c435e4f0c05df1
Alex-Trauthman <alextrauthman.full@gmail.com>
2024-05-14 22:49:05 -0300

    comments on posts

--- src/main/java/com/alex/springmongodb/config/Instantiation.java
+++ src/main/java/com/alex/springmongodb/config/Instantiation.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Configuration;
 import com.alex.springmongodb.domain.Post;
 import com.alex.springmongodb.domain.User;
 import com.alex.springmongodb.dto.AuthorDTO;
+import com.alex.springmongodb.dto.CommentDTO;
 import com.alex.springmongodb.repositories.PostRepository;
 import com.alex.springmongodb.repositories.UserRepository;
 
@@ -35,6 +36,13 @@ public class Instantiation implements CommandLineRunner {
 		
 		Post pos1 = new Post(null, sdf.parse("21/03/2018"), "Partiu sp", "vo pra sp", new AuthorDTO(maria));
 		Post pos2 = new Post(null, sdf.parse("11/05/2024"), "Enchentes no rs", "Doem por favor!", new AuthorDTO(alex));
+		CommentDTO c1 = new CommentDTO("boa viagem",sdf.parse("22/03/2018"),new AuthorDTO(alex));
+		CommentDTO c2 = new CommentDTO("aproveite !",sdf.parse("23/03/2018"),new AuthorDTO(bob));
+		CommentDTO c3 = new CommentDTO("poxa :(",sdf.parse("12/05/2024"),new AuthorDTO(maria));
+		
+		pos1.getComments().addAll(Arrays.asList(c1,c2));
+		pos2.getComments().add(c3);
+		
 		postRepository.saveAll(Arrays.asList(pos1,pos2));
 		
 		maria.addPost(pos1);
--- src/main/java/com/alex/springmongodb/domain/Post.java
+++ src/main/java/com/alex/springmongodb/domain/Post.java
@@ -1,13 +1,16 @@
 package com.alex.springmongodb.domain;
 
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 import java.util.Objects;
 
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import com.alex.springmongodb.dto.AuthorDTO;
+import com.alex.springmongodb.dto.CommentDTO;
 @Document
 public class Post implements Serializable{
 	/**
@@ -21,6 +24,8 @@ public class Post implements Serializable{
 	private String body;
 	
 	private AuthorDTO author;
+	
+	private List<CommentDTO> comments = new ArrayList<>();
 	
 	public AuthorDTO getAuthor() {
 		return author;
@@ -77,6 +82,12 @@ public class Post implements Serializable{
 			return false;
 		Post other = (Post) obj;
 		return Objects.equals(id, other.id);
+	}
+	public List<CommentDTO> getComments() {
+		return comments;
+	}
+	public void setComments(List<CommentDTO> comments) {
+		this.comments = comments;
 	}
 	
 
