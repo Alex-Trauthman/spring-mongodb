c928d40cc76cc98f93b4980566f0d1c337688c29
Alex-Trauthman <alextrauthman.full@gmail.com>
2024-05-15 00:02:24 -0300

    searching posts by content and date

--- src/main/java/com/alex/springmongodb/repositories/PostRepository.java
+++ src/main/java/com/alex/springmongodb/repositories/PostRepository.java
@@ -1,12 +1,16 @@
 package com.alex.springmongodb.repositories;
 
+import java.util.Date;
 import java.util.List;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.alex.springmongodb.domain.Post;
 @Repository
 public interface PostRepository extends MongoRepository<Post, String> {
 	List<Post> findByTitleContainingIgnoreCase(String title);
+	@Query("{ $and: [ { date: {$gte: ?1} }, { date: { $lte: ?2} } , { $or: [ { 'title': { $regex: ?0, $options: 'i' } }, { 'body': { $regex: ?0, $options: 'i' } }, { 'comments.text': { $regex: ?0, $options: 'i' } } ] } ] }")
+	List<Post> fullSearch(String text, Date minDate, Date maxDate);
 }
